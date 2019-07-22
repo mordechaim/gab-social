b1e6b63d1fe908840aa6d3ae068a9eff4909cbca
Rob Colbert <rob@gab.com>
2019-07-21 23:50:09 -0400

    migration

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # == Schema Information
 #
-# Table name: favorites
+# Table name: favourites
 #
 #  id         :bigint(8)        not null, primary key
 #  created_at :datetime         not null
