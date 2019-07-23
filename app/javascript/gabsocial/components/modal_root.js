b61596b1a759e0c99e7146658f11db5c841fa143
mgabdev <>
2019-07-22 23:10:05 -0400

    Added confirmation to compose modal on close if has text
    Added to modal_root if modal type is compose
    Added to compose_modal
    Fixes: #51

--- app/javascript/gabsocial/components/modal_root.js
+++ app/javascript/gabsocial/components/modal_root.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { injectIntl, FormattedMessage, defineMessages } from 'react-intl';
+import { connect } from 'react-redux';
+import { openModal } from '../actions/modal';
+import { cancelReplyCompose } from '../actions/compose';
 
-export default class ModalRoot extends React.PureComponent {
+const messages = defineMessages({
+  confirm: { id: 'confirmations.delete.confirm', defaultMessage: 'Delete' },
+});
+
+const mapStateToProps = state => ({
+  composeText: state.getIn(['compose', 'text']),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onOpenModal(type, opts) {
+    dispatch(openModal(type, opts));
+  },
+  onCancelReplyCompose() {
+    dispatch(cancelReplyCompose());
+  }
+});
+
+class ModalRoot extends React.PureComponent {
 
   static propTypes = {
     children: PropTypes.node,
     onClose: PropTypes.func.isRequired,
+    onOpenModal: PropTypes.func.isRequired,
+    onCancelReplyCompose: PropTypes.func.isRequired,
+    intl: PropTypes.object.isRequired,
+    composeText: PropTypes.string,
+    type: PropTypes.string,
   };
 
   state = {
@@ -17,9 +43,25 @@ export default class ModalRoot extends React.PureComponent {
   handleKeyUp = (e) => {
     if ((e.key === 'Escape' || e.key === 'Esc' || e.keyCode === 27)
          && !!this.props.children) {
-      this.props.onClose();
+      this.handleOnClose();
     }
   }
+
+  handleOnClose = () => {
+    const { onOpenModal, composeText, onClose, intl, type, onCancelReplyCompose } = this.props;
+
+    if (composeText && type == 'COMPOSE') {
+      onOpenModal('CONFIRM', {
+        message: <FormattedMessage id='confirmations.delete.message' defaultMessage='Are you sure you want to delete this status?' />,
+        confirm: intl.formatMessage(messages.confirm),
+        onConfirm: () => onCancelReplyCompose(),
+        onCancel: () => onOpenModal('COMPOSE'),
+      });
+    }
+    else {
+      this.props.onClose();
+    }
+  };
 
   componentDidMount () {
     window.addEventListener('keyup', this.handleKeyUp, false);
@@ -63,7 +105,7 @@ export default class ModalRoot extends React.PureComponent {
   }
 
   render () {
-    const { children, onClose } = this.props;
+    const { children } = this.props;
     const { revealed } = this.state;
     const visible = !!children;
 
@@ -76,11 +118,12 @@ export default class ModalRoot extends React.PureComponent {
     return (
       <div className='modal-root' ref={this.setRef} style={{ opacity: revealed ? 1 : 0 }}>
         <div style={{ pointerEvents: visible ? 'auto' : 'none' }}>
-          <div role='presentation' className='modal-root__overlay' onClick={onClose} />
+          <div role='presentation' className='modal-root__overlay' onClick={() => this.handleOnClose()} />
           <div role='dialog' className='modal-root__container'>{children}</div>
         </div>
       </div>
     );
   }
-
 }
+
+export default injectIntl(connect(mapStateToProps, mapDispatchToProps)(ModalRoot));
