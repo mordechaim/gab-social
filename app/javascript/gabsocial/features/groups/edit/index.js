47a18f60240e1b9c5610b3f927249e0883bdf28c
Dank Gabs <dankgabs@protonmail.com>
2019-07-22 22:35:46 -0400

    Adjustments to the create group form to match mockup specs. Strip out css for form input from component css file. Created global form input css. Padding adjustments.

--- app/javascript/gabsocial/features/groups/edit/index.js
+++ app/javascript/gabsocial/features/groups/edit/index.js
@@ -106,7 +106,8 @@ class Edit extends React.PureComponent {
 			<form className='group-form' onSubmit={this.handleSubmit}>
 				<div>
 					<input
-						className='group-form__input'
+						className='standard'
+						type='text'
 						value={title}
 						disabled={disabled}
 						onChange={this.handleTitleChange}
@@ -116,7 +117,8 @@ class Edit extends React.PureComponent {
 					
 				<div>
 					<input
-						className='group-form__input'
+						className='standard'
+						type='text'
 						value={description}
 						disabled={disabled}
 						onChange={this.handleDescriptionChange}
