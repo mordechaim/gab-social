c832fb6cbc4ce446306addab0e66ffeb35bfb231
2458773093 <2458773093@protonmail.com>
2019-08-01 01:16:22 +0300

    New option to notificaion preferences for emails from gab.com for announcements and other information

--- app/controllers/settings/notifications_controller.rb
+++ app/controllers/settings/notifications_controller.rb
@@ -25,7 +25,7 @@ class Settings::NotificationsController < Settings::BaseController
 
   def user_settings_params
     params.require(:user).permit(
-      notification_emails: %i(follow follow_request reblog favourite mention digest report pending_account),
+      notification_emails: %i(follow follow_request reblog favourite mention digest report pending_account emails_from_gabcom),
       interactions: %i(must_be_follower must_be_following must_be_following_dm)
     )
   end
