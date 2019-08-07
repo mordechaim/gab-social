05037f60be7b3733b12d67b4d9f237d0ad43459b
Rob Colbert <rob@gab.com>
2019-08-07 15:47:34 -0400

    Remove investing information
    - Removed "Invest" from unauthenticated home page in Navbar
    - Removed button/link from promo area of authenticated home page in
    sidebar
    - Updated Investors page to remove link leaving two beyind (reports)

--- app/javascript/gabsocial/features/ui/components/promo_panel.js
+++ app/javascript/gabsocial/features/ui/components/promo_panel.js
@@ -13,19 +13,6 @@ const PromoPanel = () => (
     <div className='promo-panel__container'>
 
       <div className='promo-panel-item'>
-        <a className='promo-panel-item__btn button button-alternative-2' href='https://invest.gab.com'>
-          <Icon id='check-circle' className='promo-panel-item__icon' fixedWidth />
-          <FormattedMessage id='promo.invest_heading' defaultMessage='Invest in Gab' />
-        </a>
-        <p className='promo-panel-item__message'>
-          <FormattedMessage
-            id='promo.invest_message'
-            defaultMessage='Learn more about investing in Gab and our vision for the future.'
-          />
-        </p>
-      </div>
-
-      <div className='promo-panel-item'>
         <a className='promo-panel-item__btn button button-alternative-2' href='/invites'>
           <Icon id='envelope' className='promo-panel-item__icon' fixedWidth />
           <FormattedMessage id='promo.invite_heading' defaultMessage='Invite Friends' />
